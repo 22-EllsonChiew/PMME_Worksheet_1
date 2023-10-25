5e138eda65e808010469fc5b56c8d1bc1b9cc4c6
ItzUzume <ellson637@gmail.com>
2023-10-25 17:24:50 +0800

    Update 4(finish all Cam only left TopDown0

--- PMME_Worksheet_!/Assets/Resoruces/Scripts/ThirdPersonCamera.cs
+++ PMME_Worksheet_!/Assets/Resoruces/Scripts/ThirdPersonCamera.cs
@@ -1,9 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+public static class GameConstants
+{
+    public static Vector3 CameraAngleOffset { get; set; }
+    public static Vector3 CameraPositionOffset { get; set; }
 
+    public static float Damping { get; set; }
+}
 public class ThirdPersonCamera : MonoBehaviour
 {
+
+    public Vector3 mAngleOffset = new Vector3(0.0f, 0.0f, 0.0f);
+
+    public float mDamping = 1.0f;
+
+    public Vector3 mPositionOffset = new Vector3(0.0f, 2.0f, -2.5f);
+
+
     public abstract class TPCBase
     {
         protected Transform mCameraTransform;
@@ -39,6 +53,8 @@ public class ThirdPersonCamera : MonoBehaviour
         public override void Update()
         {
             Vector3 targetPos = mPlayerTransform.position;
+
+            targetPos.y += GameConstants.CameraPositionOffset.y;
             mCameraTransform.LookAt(targetPos);
         }
     }
@@ -49,6 +65,16 @@ public class ThirdPersonCamera : MonoBehaviour
     private void Start()
     {
         mThirdPersonCamera = new TPCTrack(transform, mPlayer);
+
+        GameConstants.Damping = mDamping;
+        GameConstants.CameraPositionOffset = mPositionOffset;
+        GameConstants.CameraAngleOffset = mAngleOffset;
+
+        GameConstants.CameraPositionOffset = mPositionOffset;
+        //mThirdPersonCamera = new TPCTrack(transform, mPlayer);
+        //mThirdPersonCamera = new TPCFollowTrackPosition(transform, mPlayer);
+
+        mThirdPersonCamera = new TPCFollowTrackPositionAndRotation(transform, mPlayer);
     }
 
     private void LateUpdate()
@@ -56,4 +82,67 @@ public class ThirdPersonCamera : MonoBehaviour
         mThirdPersonCamera.Update();
     }
 
+    public abstract class TPCFollow : TPCBase
+    {
+        public TPCFollow(Transform cameraTransform, Transform PlayerTransform) : base(cameraTransform, PlayerTransform)
+        {
+
+        }
+
+        public override void Update()
+        {
+            Vector3 forward = mPlayerTransform.forward;
+            Vector3 right = mPlayerTransform.right;
+            Vector3 up = mPlayerTransform.up;
+
+            Vector3 targetPos = mPlayerTransform.position;
+
+            Vector3 desiredPosition = targetPos + GameConstants.CameraPositionOffset;
+
+            Vector3 position = Vector3.Lerp(mCameraTransform.position, desiredPosition, Time.deltaTime * GameConstants.Damping);
+            mCameraTransform.position = position;
+        }
+    }
+
+    public class TPCFollowTrackPosition : TPCFollow
+    {
+        public TPCFollowTrackPosition(Transform cameraTransform, Transform
+        playerTransform)
+        : base(cameraTransform, playerTransform)
+        {
+        }
+        public override void Update()
+        {
+            
+            Quaternion initialRotation =
+            Quaternion.Euler(GameConstants.CameraAngleOffset);
+            
+            mCameraTransform.rotation =
+            Quaternion.RotateTowards(mCameraTransform.rotation,
+            initialRotation,
+            Time.deltaTime * GameConstants.Damping);
+            
+            base.Update();
+        }
+    }
+
+    public class TPCFollowTrackPositionAndRotation : TPCFollow
+    {
+        public TPCFollowTrackPositionAndRotation(Transform cameraTransform, Transform playerTransform): base(cameraTransform, playerTransform)
+        {
+
+        }
+
+        public override void Update()
+        {
+            Quaternion initalRotation = Quaternion.Euler(GameConstants.CameraAngleOffset);
+
+            mCameraTransform.rotation = Quaternion.Lerp(mCameraTransform.rotation, mPlayerTransform.rotation * initalRotation, Time.deltaTime * GameConstants.Damping);
+
+            base.Update();
+        }
+    }
+
+
+    
 }
